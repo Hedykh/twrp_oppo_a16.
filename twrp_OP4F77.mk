ed956c90ba1be33ace5066df6fd13ad65b1cf0ab
Hedykh <heedikh3@gmail.com>
2026-04-16 22:25:10 +0000

    Initial commit

---
+++ twrp_OP4F77.mk
@@ -0,0 +1,12 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom device configuration
+$(call inherit-product, device/oppo/OP4F77/device.mk)
+
+PRODUCT_DEVICE := OP4F77
+PRODUCT_NAME := twrp_OP4F77
+PRODUCT_BRAND := Oppo
+PRODUCT_MODEL := Oppo A16
+PRODUCT_MANUFACTURER := oppo
